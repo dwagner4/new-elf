ea0eb2f0ff73cb1b8cde7a5701f34fc0ab832bc8
Dean Wagner <deanwagner13@gmail.com>
2022-05-18 08:40:36 -0400

    Initial commit

---
+++ js/scenery/HeartScenery.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import Scenery from '../systems/Scenery.js';
+
+export default class HeartScenery extends Scenery {
+  constructor() {
+    super();
+    this.hemilight = new THREE.HemisphereLight(0xff8800, 0xffffff, 1);
+
+    this.light = new THREE.DirectionalLight(0xffffff, 0.5);
+    this.light.position.set(1, 1, 1).normalize();
+    this.light.castShadow = true;
+    this.light.shadow.mapSize.width = 512;
+    this.light.shadow.mapSize.height = 512;
+    this.light.shadow.camera.near = 0.5;
+    this.light.shadow.camera.far = 500;
+
+    const geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xffdd88,
+      side: THREE.DoubleSide,
+    });
+    this.plane = new THREE.Mesh(geometry, material);
+    this.plane.rotateX(-Math.PI / 2);
+    this.plane.translateZ(-1);
+    this.plane.receiveShadow = true;
+  }
+}
